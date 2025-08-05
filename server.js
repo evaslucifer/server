23e3567c821a16d1eebe9796e3545d3c1a4c3bf8
evaslucifer <himanish772005@gmail.com>
2025-08-05 21:38:03 +0530

    hello

--- server.js
+++ server.js
@@ -1,39 +1,11 @@
-const http=require("http");
+const http = require("http");
+const port = 8081;
 
-const port=8081
-;
-// http methods
-/*
->> get :inorder to get data from server
- >>post : sending data server 
- >>delete: deleting the data from the database
- >>patch: updating certain fields
- >>put: full update 
- */
-
- const toDoList=["learn","apply","succed"];
-
-
- http.createServer((req,res)=>{
-    const{method,url}=req;
-    // console.log(method,url);
-    if(url==="/todos"){
-        if(method==="GET"){
-            res.writeHead(200);
-            res.write(toDoList.toString());
-        }
-
-    }
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>Hey, server started</h2>");
     res.end();
-    // res.writeHead(200,{"content-type":"text/html"});
-    // res.write("<h2>hey server started-1234</h2>");
-    // res.end;
-
 })
-.listen(port,()=>{
-    console.log(`node js server has started running on port ${port}`);
-
+.listen(port, () => {
+    console.log(`Your server started on port ${port}`);
 });
-
-
-    
